1ab03d48db1977d08a86d4e97df4e54dbfe1179d
seedgou <i@zczc.cz>
2017-12-18 08:50:06 +0800

    size_info_analyzer

--- analyzer/size_info_analyzer.py
+++ analyzer/size_info_analyzer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+import matplotlib.pyplot as plt
+from numpy import sqrt, ceil, floor
+
 from utils.utils import *
 
 
@@ -8,7 +11,11 @@ class SizeInfoAnalyzer:
     """ 爬取淘宝手机商品记录 """
 
     def __init__(self, keywords, classifiers):
-        keywords = {
+        """
+        initialize SizeInfoAnalyzer
+
+        :param keywords: keywords example:
+        {
             '小米': ['米', 'mi'],
             '苹果': ['苹果', 'apple', 'iphone'],
             '三星': ['三星', 'samsung'],
@@ -16,8 +23,9 @@ class SizeInfoAnalyzer:
             'OPPO': ['oppo'],
             '华为': ['huawei', '华为']
         }
-        classifiers = ['16G', '32G', '64G', '128G', '256G']
-
+        :param classifiers: classifiers example:
+        ['16G', '32G', '64G', '128G', '256G']
+        """
         self.keywords = keywords
         self.classifiers = classifiers
         self.client, self.db = init_client()
@@ -64,7 +72,38 @@ class SizeInfoAnalyzer:
                         self.rates_count[k][classifier] += 1
                         break
 
+    def __pie_sub_plot(self, ax, data, title):
+        keys = list(data.keys())
+        values = list(data.values())
+        labels = keys
+        sizes = values
+
+        patches, l_text, p_text = ax.pie(sizes, labels=labels, labeldistance=1.1, autopct='%2.1f%%', shadow=False,
+                                         startangle=90, pctdistance=0.6)
+        for t in l_text:
+            t.set_size = (30)
+        for t in p_text:
+            t.set_size = (20)
+        ax.axis('equal')
+        ax.legend()
+        ax.set_title(title)
+
+    def __save_pie_plot(self):
+        scale = 5
+        fig, axes = plt.subplots(nrows=int(floor(sqrt(len(self.rates_count)))),
+                                 ncols=int(ceil(sqrt(len(self.rates_count)))),
+                                 figsize=(
+                                     int(ceil(sqrt(len(self.rates_count)))) * scale,
+                                     int(floor(sqrt(len(self.rates_count)))) * scale))
+        count = 0
+        for ax_row in axes:
+            for ax in ax_row:
+                key, value = list(self.rates_count.items())[count]
+                self.__pie_sub_plot(ax, value, key)
+                count += 1
+        fig.savefig('size_info_analyzer.pdf')
+
     def run(self):
         self.__read_rates_by_brand()
         self.__count_by_classifier()
-        print(self.rates_count)
+        self.__save_pie_plot()
